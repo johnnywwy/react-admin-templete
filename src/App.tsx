3545a92756b0232d869f5b82e6bb788d66a03368
johnnywwy <johnnywwy@gmail.com>
2025-06-25 14:51:44 +0800

    feat: startTransition

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState, useTransition } from "react";
 // import './App.css'
 import UseEffectDemo from "./hooks/UseEffect";
 import UseStateDemo from "./hooks/UseState";
@@ -6,13 +6,34 @@ import UseEffectFetch from "./hooks/UseEffect/request";
 import { UseReducer, UseReducerDemo } from "./hooks/UseReducer";
 import { useStorage } from "./hooks/UseStroage";
 import { useHistory } from "./hooks/UseHistory";
+import { Input, List } from "antd";
 
-UseEffectFetch;
+interface Iitem {
+  id: string;
+  name: string;
+  address: string;
+  age: number;
+}
+
 function App() {
   const [val, setVal] = useStorage("data", 1);
 
   const [url, push, replace] = useHistory();
 
+  const [list, setList] = useState<Iitem[]>([]);
+  const [inputValue, setInputValue] = useState("");
+  const [isPending, startTransition] = useTransition();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+    setInputValue(val);
+    fetch("/api/mock/list?key=" + val)
+      .then((res) => res.json())
+      .then((res) => {
+        startTransition(() => {
+          setList(res.list);
+        });
+      });
+  };
   return (
     <>
       {/* <UseStateDemo /> */}
@@ -24,11 +45,25 @@ function App() {
       {/* <h3>{val}</h3> */}
       {/* <button onClick={() => setVal(val + 1)}>设置val</button> */}
 
-      <hr />
+      {/* <hr />
       <h2>当前url：{url}</h2>
       <button onClick={() => push("/xxxx")}>push</button>
       <button onClick={() => replace("/yyyy")}>replace</button>
+      <hr /> */}
+
       <hr />
+      <Input value={inputValue} onChange={handleChange} />
+      {isPending && <div>loading...</div>}
+      <List
+        dataSource={list}
+        renderItem={(item) => (
+          <List.Item>
+            {item.name}
+            <br />
+            {item.address}
+          </List.Item>
+        )}
+      />
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import type { Plugin } from 'vite';
+import mockjs from 'mockjs';
+import url from 'node:url';
+
+// vite插件
+const viteMockServer = (): Plugin => {
+  return {
+    name: 'vite-plugin-mock',
+    configureServer: (server) => {
+      server.middlewares.use('/api/mock/list', (req, res) => {
+        const parseurl = url.parse(req.originalUrl, true).query;
+        res.setHeader('Content-Type', 'application/json');
+        const data = mockjs.mock({
+          'list|2000': [
+            {
+              id: '@guid',
+              // name: '@cword(2, 4)',
+              name: parseurl.key,
+              age: '@integer(18, 30)',
+              address: '@county(true)',
+            },
+          ],
+        });
+        res.end(JSON.stringify(data))
+      })
+    }
+  }
+}
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteMockServer()],
 })
